95e1352d35e192b2249e1e1afe20674ed6a17d86
NewGen2022 <amirgimari@gmail.com>
2024-05-11 13:22:17 +0300

    deleted src/index.js ans src/test.js

---
+++ src/factories/Gameboard.js
@@ -0,0 +1,26 @@
+class GameBoard {
+    constructor () {
+
+    }
+
+    init () {
+
+    }
+
+    placeShip () {
+
+    }
+
+    // takes a pair of coordinates, determines whether or 
+    // not the attack hit a ship and then sends the ‘hit’ 
+    // function to the correct ship, or records the 
+    // coordinates of the missed shot.
+    receiveAttack () {
+
+    }
+
+
+    isGameOver () {
+
+    }
+}
--- src/test.js
+++
@@ -1,5 +0,0 @@
-export default function log () {
-
-    alert(1234);
-
-}
