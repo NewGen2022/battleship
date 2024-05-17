878323a8294315a904a58e908b27d93432bd5c6f
NewGen2022 <amirgimari@gmail.com>
2024-05-17 20:54:19 +0300

    added class for generating first page

---
+++ src/appearance/firstPage.js
@@ -0,0 +1,86 @@
+import swords from '../assets/sword.png';
+import rightArrow from '../assets/right-arrow.png';
+import { createBackOrNextButton } from './hideDisplayPages';
+ 
+class FirstPage {
+    constructor () {
+        this.startMain = document.createElement('div');
+        this.startMain.setAttribute('id', 'start-screen-container');
+
+        this.generateFirstStartPage();
+    }
+
+    // creates first start screen page with
+    // choice between PlayerVSPlayer and PlayerVSBot
+    generateFirstStartPage () {
+        const body = document.querySelector("body");
+        body.appendChild(this.startMain);
+
+        this.generatePlayerOrBotChoice();
+        createBackOrNextButton(rightArrow);
+        this.handlePlayerOrBotChange();
+    }
+
+    // creates PlayerVSPlayer and PlayerVSBot containers
+    // for first page
+    generatePlayerOrBotChoice() {
+        const playerOrBotContainer = document.createElement('div');
+        const playerVsPlayer = this.#createPlayerOption('Player', 'Player');
+        const or = document.createElement('div');
+        const playerVsRobot = this.#createPlayerOption('Player', 'Bot');
+
+        playerOrBotContainer.setAttribute('id', 'player-or-bot-container');
+        playerVsPlayer.classList.add('active-player-vs-bot');
+
+        or.textContent = 'OR';
+        or.setAttribute('id', 'or');
+    
+        playerOrBotContainer.appendChild(playerVsPlayer);
+        playerOrBotContainer.appendChild(or);
+        playerOrBotContainer.appendChild(playerVsRobot);
+    
+        this.startMain.appendChild(playerOrBotContainer);
+    }
+
+    // create options of WhoVSWho according to the 
+    // passed attributes for first page
+    #createPlayerOption(player1, player2) {
+        const playerOption = document.createElement('div');
+        const playerText1 = document.createTextNode(player1);
+        const playerText2 = document.createTextNode(player2);
+        const vsImg = document.createElement('img');
+
+        playerOption.setAttribute('id', `${player1.toLowerCase()}-vs-${player2.toLowerCase()}`);
+    
+        vsImg.src = swords;
+    
+        playerOption.appendChild(playerText1);
+        playerOption.appendChild(vsImg);
+        playerOption.appendChild(playerText2);
+    
+        return playerOption;
+    }
+
+    // adds event listeners to PlayerVSPlayer and PlayerVSBot 
+    // to change the class of the active one for first page
+    handlePlayerOrBotChange () {
+        const playerVsPlayer = document.getElementById('player-vs-player');
+        const playerVsBot = document.getElementById('player-vs-bot');
+
+        playerVsPlayer.addEventListener('click', () => {
+            if (playerVsPlayer.class === undefined) {
+                playerVsBot.classList.remove('active-player-vs-bot')
+                playerVsPlayer.classList.add('active-player-vs-bot');
+            }
+        })
+
+        playerVsBot.addEventListener('click', () => {
+            if (playerVsBot.class === undefined) {
+                playerVsPlayer.classList.remove('active-player-vs-bot')
+                playerVsBot.classList.add('active-player-vs-bot');
+            }
+        })
+    }
+}
+
+export default FirstPage;
