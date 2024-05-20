b74c5fe9c1112455da2b1f665c997d053206857f
NewGen2022 <amirgimari@gmail.com>
2024-05-20 09:46:51 +0300

    added gameboard displaying

--- src/appearance/secondPage.js
+++ src/appearance/secondPage.js
@@ -1,9 +1,16 @@
 import leftArrow from '../assets/left-arrow.png';
 import { createBackOrNextButton } from './hideDisplayPages';
+import GameBoard from '../factories/Gameboard';
 
 class SecondPage {
     constructor () {
         this.startMain = document.getElementById('start-screen-container');
+
+        this.leftBoard = document.createElement('div');
+        this.rightBoard = document.createElement('div');
+
+        this.leftBoardLogic = new GameBoard();
+        this.rightBoardLogic = new GameBoard();
 
         this.inputs = document.createElement('div');
         this.leftInput = document.createElement('div');
@@ -12,6 +19,12 @@ class SecondPage {
         this.inputs.setAttribute('id', 'inputs');
         this.leftInput.setAttribute('id', 'left-input');
         this.rightInput.setAttribute('id', 'right-input');
+        
+        this.leftBoard.setAttribute('id', 'left-board');
+        this.rightBoard.setAttribute('id', 'right-board');
+
+        console.log(this.leftBoardLogic);
+        console.log(this.rightBoardLogic);
 
         this.startMain.appendChild(this.inputs);
 
@@ -44,6 +57,72 @@ class SecondPage {
         this.inputs.appendChild(this.leftInput);
         this.inputs.appendChild(this.rightInput);
     }
+
+    displayGameBoard () {
+        const player2 = document.getElementById('player2-input');
+
+        this.displayLeftBoard();
+        this.displayRightBoard();
+        
+        if (!player2) {
+            const rightBoard = document.getElementById('right-board');
+            const rightBoardCells = rightBoard.querySelectorAll('.board-cell');
+            rightBoardCells.forEach(cell => {
+                cell.classList.add('disable-board');
+            });
+        }
+    }
+
+    displayLeftBoard() {
+        this.leftBoard.innerHTML = ''; // Clear previous board display
+        this.leftBoardLogic.board.forEach((row, rowIndex) => {
+            const rowDiv = document.createElement('div');
+            rowDiv.classList.add('board-row');
+    
+            row.forEach((cell, colIndex) => {
+                const cellDiv = document.createElement('div');
+                cellDiv.classList.add('board-cell');
+    
+                if (cell) {
+                    cellDiv.classList.add('ship-cell'); // Ship is present
+                } else if (this.leftBoardLogic.missedShots[rowIndex][colIndex]) {
+                    cellDiv.classList.add('missed-cell'); // Missed shot
+                }
+    
+                rowDiv.appendChild(cellDiv);
+            });
+    
+            this.leftBoard.appendChild(rowDiv);
+        });
+
+        this.leftInput.appendChild(this.leftBoard);
+    }
+
+    displayRightBoard() {
+        this.rightBoard.innerHTML = ''; // Clear previous board display
+        this.rightBoardLogic.board.forEach((row, rowIndex) => {
+            const rowDiv = document.createElement('div');
+            rowDiv.classList.add('board-row');
+    
+            row.forEach((cell, colIndex) => {
+                const cellDiv = document.createElement('div');
+                cellDiv.classList.add('board-cell');
+    
+                if (cell) {
+                    cellDiv.classList.add('ship-cell'); // Ship is present
+                } else if (this.rightBoardLogic.missedShots[rowIndex][colIndex]) {
+                    cellDiv.classList.add('missed-cell'); // Missed shot
+                }
+    
+                rowDiv.appendChild(cellDiv);
+            });
+    
+            this.rightBoard.appendChild(rowDiv);
+        });
+
+        this.rightInput.appendChild(this.rightBoard);
+    }
+    
 
     #createNameInput (player) {
         const input = document.createElement('input');
